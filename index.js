47dea2bd0179a7ccef0a7b15027612f2bbaa9635
Alan Contreras <alancontreras74@gmail.com>
2026-04-29 00:18:25 -0300

    fix express path-to-regexp

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
   //origin: ['http://localhost:3000', 'https://entrega-final-tt-react-js.vercel.app'], // dominios permitidos
-  origin: ['https://localhost:3000', 'https://e-commerce-cero-huella.vercel.app/'], // Permitir solicitudes desde cualquier origen (no recomendado para producción)
+  origin: ['https://localhost:3000', 'https://e-commerce-cero-huella.vercel.app'], // Permitir solicitudes desde cualquier origen (no recomendado para producción)
   methods: ['GET', 'POST', 'PUT', 'DELETE'],                  // métodos permitidos
   allowedHeaders: ['Content-Type', 'Authorization'],          // cabeceras permitidas
   exposedHeaders: ['Content-Length'],                         // cabeceras visibles al cliente
