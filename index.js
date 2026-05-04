98f6256d37b287f9f5ec50fb42155be014e1826c
Alan Contreras <alancontreras74@gmail.com>
2026-05-03 22:02:30 -0300

    cambios

--- index.js
+++ index.js
@@ -64,8 +64,7 @@ app.use((req, res, next) => {
   res.status(404).json({ error: -2, description: `Ruta ${req.originalUrl}  No Encontrada` });
   next();  
 });
-console.log(__dirname);
-console.log(join(__dirname, "public/index.html"));
+
 //listeners
 
 //app.set("PORT", 3000); // UNA FORMA DE EXPRESS DE SETEAR UN PUERTO FIJO
